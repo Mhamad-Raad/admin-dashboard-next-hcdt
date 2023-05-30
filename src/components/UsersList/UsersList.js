c0d5378dc08a258c954163e91f4b21a08abeb9bc
joker69216 <hamaraad883@gmail.com>
2023-05-30 23:12:25 +0300

    add styles to the table

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -2,6 +2,27 @@ import css from './UsersList.module.css';
 
 export default function UsersList() {
   return (
-    <div>UsersList</div>
-  )
+    <table className={css.users_table}>
+      <tr>
+        <th></th>
+        <th>ID</th>
+        <th>Avatar</th>
+        <th>Email</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Actions</th>
+      </tr>
+      <tr>
+        <td>
+          <input type='checkbox' className={css.table_checkbox} />
+        </td>
+        <td>1</td>
+        <td>img</td>
+        <td>hama@gmail.com</td>
+        <td>Mhamad</td>
+        <td>Raad</td>
+        <td>wait something long here like action</td>
+      </tr>
+    </table>
+  );
 }
