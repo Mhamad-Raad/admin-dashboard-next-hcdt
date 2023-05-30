7ee5f54ec1dfa4b904427a97fbecee76927aebf5
joker69216 <hamaraad883@gmail.com>
2023-05-30 19:21:43 +0300

    make navbar structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
-'use client';
-import { Box } from '@chakra-ui/react';
+
 
 export default function Home() {
   return (
     <div>
       <h1>hello world</h1>
-      {<Box maxW='960px' mx='auto' />}
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+'use client';
+import { Switch, Flex, FormControl} from '@chakra-ui/react';
+
+export default function Navbar() {
+  return (
+    <nav>
+      <Flex w='100%' h='81px' bg='#F9FAFA'>
+        
+      </Flex>
+    </nav>
+  );
+}
