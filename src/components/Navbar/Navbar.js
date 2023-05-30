398fca37c4ac6327d90bdf1e03381291fdef9805
joker69216 <hamaraad883@gmail.com>
2023-05-31 02:03:41 +0300

    add theme switch

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,27 @@
 'use client';
-import { Flex} from '@chakra-ui/react';
+import {useState} from 'react'
+import { Flex } from '@chakra-ui/react';
+
+import { DarkModeSwitch } from 'react-toggle-dark-mode';
+
+import css from './Navbar.module.css';
 
 export default function Navbar() {
+
+  const [isDarkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = (checked) => {
+    setDarkMode(checked);
+  };
+
   return (
     <nav>
-      <Flex w='100%' h='81px' bg='#F9FAFA'>
-
+      <Flex w='100%' h='81px' bg='#F9FAFA' alignItems="center" paddingLeft="95.26px">
+        <DarkModeSwitch
+          checked={isDarkMode}
+          onChange={toggleDarkMode}
+          size={40}
+        />
       </Flex>
     </nav>
   );
