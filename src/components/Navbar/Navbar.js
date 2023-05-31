029837864e631b329b7e3254d385580c2c7ef4fb
joker69216 <hamaraad883@gmail.com>
2023-05-31 22:24:03 +0300

    add dynamic routing

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,9 @@
 'use client';
 import { useState } from 'react';
-import { Flex } from '@chakra-ui/react';
+import Link from 'next/link';
 import { useSelector } from 'react-redux';
+
+import { Flex } from '@chakra-ui/react';
 import { DarkModeSwitch } from 'react-toggle-dark-mode';
 
 import css from './Navbar.module.css';
@@ -33,19 +35,21 @@ export default function Navbar() {
           size={40}
         />
         {userSlice.length > 0 && (
-          <Flex justify='flex-start' align='center'>
-            <img
-              src={userSlice[0].avatar}
-              alt='user avatar'
-              className={css.header_profile_avatar}
-            />
-            <Flex direction='column' justify='flex-start' align='flex-start'>
-              <p
-                className={css.header_profile_name}
-              >{`${userSlice[0].first_name} ${userSlice[0].last_name}`}</p>
-              <p className={css.header_profile_email}>{userSlice[0].email}</p>
+          <Link href="/posts/first-post">
+            <Flex justify='flex-start' align='center'>
+              <img
+                src={userSlice[0].avatar}
+                alt='user avatar'
+                className={css.header_profile_avatar}
+              />
+              <Flex direction='column' justify='flex-start' align='flex-start'>
+                <p
+                  className={css.header_profile_name}
+                >{`${userSlice[0].first_name} ${userSlice[0].last_name}`}</p>
+                <p className={css.header_profile_email}>{userSlice[0].email}</p>
+              </Flex>
             </Flex>
-          </Flex>
+          </Link>
         )}
       </Flex>
     </nav>
