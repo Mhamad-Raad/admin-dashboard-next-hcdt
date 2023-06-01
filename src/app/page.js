ff9a9db6311d6038db0f1771880fbfe987fd423c
joker69216 <hamaraad883@gmail.com>
2023-06-01 16:45:20 +0300

    add functionality to the pagination

--- src/app/page.js
+++ src/app/page.js
@@ -1,47 +1,64 @@
 'use client';
+import useSWR from 'swr';
+import ReactPaginate from 'react-paginate';
+import { Flex } from '@chakra-ui/react';
+import {  useRouter, useSearchParams, usePathname } from 'next/navigation';
 
 import HomeHeader from '../components/HomeHeader/HomeHeader';
 import UsersList from '../components/UsersList/UsersList';
 import Loading from '../components/Loading/Loading.js';
 
-import useSWR from 'swr';
-import ReactPaginate from 'react-paginate';
-import { Flex } from '@chakra-ui/react';
-
 import css from '../styles/page.module.css';
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-export default function Home() {
+export default function Home({id}) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  let search ='';
+  if(searchParams.get('page') !== null) {
+    search = +searchParams.get('page');
+  }
+
+  let URL = 'https://reqres.in/api/users'
+
+  if(search !== null && !isNaN(search)) URL += `?page=${search}`; 
+
+  console.log(URL)
+
   const { data, error, isLoading } = useSWR(
-    'https://reqres.in/api/users',
+    URL,
     fetcher
   );
 
   const handlePageClick = (event) => {
-    console.log(
-      `User requested page number ${event.selected}`
-    );
+    const params = new URLSearchParams(searchParams);
+    params.set('page', event.selected + 1);
+    router.replace(`${pathname}?${params}`);
   };
-
+console.log(data)
   return (
     <div className={css.home}>
       <HomeHeader />
       {error && !isLoading && <div>failed to load</div>}
       {isLoading && <Loading />}
-      {!isLoading && !error && <UsersList usersData={data?.data} />}
-      <Flex w='100%' justify='flex-end' align='center'>
-        <ReactPaginate
-          className={css.pagination}
-          breakLabel='...'
-          nextLabel='>'
-          onPageChange={handlePageClick}
-          pageRangeDisplayed={5}
-          pageCount={2}
-          previousLabel='<'
-          renderOnZeroPageCount={null}
-        />
-      </Flex>
+      {!isLoading && !error && <>
+          <UsersList usersData={data?.data} />
+          <Flex w='100%' justify='flex-end' align='center'>
+          <ReactPaginate
+            className={css.pagination}
+            breakLabel='...'
+            nextLabel='>'
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={3}
+            pageCount={data.total_pages}
+            previousLabel='<'
+            renderOnZeroPageCount={null}
+          />
+        </Flex>
+     </>}
+     
     </div>
   );
 }
