804756a2d0670222db5491d626acfd265ef30852
joker69216 <hamaraad883@gmail.com>
2023-06-01 13:37:13 +0300

    add create functionality

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,6 +1,6 @@
 'use client';
 
-import {useRef} from 'react'
+import { useRef, useState } from 'react';
 
 import {
   Modal,
@@ -14,53 +14,80 @@ import {
   FormControl,
   FormLabel,
   Input,
-
+  useDisclosure,
 } from '@chakra-ui/react';
-import { useDisclosure } from '@chakra-ui/react';
 
 import css from './Modal.module.css';
 
-
 export default function InitialFocus() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [fetchState, setFetchState] = useState({
+    loading: false,
+    error: false,
+    response: {},
+  });
 
   const initialRef = useRef(null);
 
+  const firstNameRef = useRef(null);
+  const lastNameRef = useRef(null);
+  const emailRef = useRef(null);
+
+  const createUser = async () => {
+    setFetchState({...fetchState, loading: true});
+    const name = `${firstNameRef.current.value} ${lastNameRef.current.value}`;
+    const email = emailRef.current.value;
+
+    // this is also fake becuase the api does not do anything
+    const response = await fetch('https://reqres.in/api/users', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        job: email
+      }),
+    }).then((res) => res.json());
+
+    console.log(response)
+  };
+
   return (
     <>
-      <Button onClick={onOpen} className={css.header_button}> <span className={css.header_button_plus}>
-          +
-        </span>
-        <span>
-          Create
-        </span></Button>
+      <Button onClick={onOpen} className={css.header_button}>
+        <span className={css.header_button_plus}>+</span>
+        <span>Create</span>
+      </Button>
 
-      <Modal
-        initialFocusRef={initialRef}
-        isOpen={isOpen}
-        onClose={onClose}
-      >
+      <Modal initialFocusRef={initialRef} isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Create your account</ModalHeader>
-          <ModalCloseButton />
+          <ModalHeader pb={2.2}>Create new user</ModalHeader>
+          <p className={css.modal_subtitle}>
+            Enter the field below and create new user{' '}
+          </p>
           <ModalBody pb={6}>
             <FormControl>
               <FormLabel>First name</FormLabel>
-              <Input ref={initialRef} placeholder='First name' />
+              <Input ref={firstNameRef} placeholder='First name' />
             </FormControl>
 
             <FormControl mt={4}>
               <FormLabel>Last name</FormLabel>
-              <Input placeholder='Last name' />
+              <Input placeholder='Last name' ref={lastNameRef} />
+            </FormControl>
+
+            <FormControl mt={4}>
+              <FormLabel>Email</FormLabel>
+              <Input placeholder='email@gmail.com' ref={emailRef} />
             </FormControl>
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme='blue' mr={3}>
-              Save
+            <Button w='100%' className={css.modal_button} onClick={createUser}>
+              Update
             </Button>
-            <Button onClick={onClose}>Cancel</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
